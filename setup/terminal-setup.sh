a916da5c57d03f7dd22dd7ab717c224f85758994
Mike Schinkel <mike@newclarity.net>
2025-05-10 20:24:46 -0400

    Add `terminal-setup.sh`

---
+++ setup/terminal-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# TODO: Comment out annoying 'echo' lines about saving sessionsm etc.
+#       in /etc/zshrc_Apple_Terminal
+# See:
+#   - https://superuser.com/questions/975678/strange-output-from-terminal-exit-command-is-this-a-virus
+#   - https://stackoverflow.com/questions/32418438/how-can-i-disable-bash-sessions-in-os-x-el-capitan
+#   - https://www.reddit.com/r/osx/comments/397uep/changes_to_bash_sessions_and_terminal_in_el/?rdt=65450
