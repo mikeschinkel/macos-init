36f4a80dd8d701b18cfdeeaa8c7749ddc8d60abb
Mike Schinkel <mike@newclarity.net>
2025-05-10 20:24:27 -0400

    Add `react-setup.sh`

---
+++ setup/react-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+#
+# See https://react.dev/learn/react-developer-tools
+
+npm install -g react-devtools
+
+react-devtools
+
+echo 'Add <script src="http://localhost:8097"></script> to your website (for Safari)'
